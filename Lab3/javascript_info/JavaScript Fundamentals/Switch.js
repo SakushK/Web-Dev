b69d4cc43a36bb0be8d11b0acc9215f84689bdb6
Kamila <98703242+SakushK@users.noreply.github.com>
2023-02-16 16:30:41 +0600

    Create Switch.js

---
+++ Lab3/javascript_info/JavaScript Fundamentals/Switch.js
@@ -0,0 +1,63 @@
+//Task 1
+
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+}
+
+//rewrite
+
+if(browser == 'Edge') {
+    alert( "You've got the Edge!" );}
+    else if (browser == 'Chrome'
+ || browser == 'Firefox'
+ || browser == 'Safari'
+ || browser == 'Opera') {
+  alert( 'Okay we support these browsers too' );
+} else {
+  alert( 'We hope that this page looks ok!' );
+}
+
+//Task 2
+
+let a = +prompt('a?', '');
+
+if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+}
+
+//rewrite
+let a = +prompt('a?', '');
+
+switch (a) {
+  case 0:
+    alert( 0 );
+    break;
+
+  case 1:
+    alert( 1 );
+    break;
+
+  case 2:
+  case 3:
+    alert( '2,3' );
+    break;
+}
