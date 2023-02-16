dc624db0e41e792d54d60feacc07c61dbc7c8291
Kamila <98703242+SakushK@users.noreply.github.com>
2023-02-16 17:19:08 +0600

    Create Weakmap.js

---
+++ Lab3/javascript_info/Data types/Weakmap.js
@@ -0,0 +1,37 @@
+//Task 1
+let messages = [
+    {text: "Hello", from: "John"},
+    {text: "How goes?", from: "John"},
+    {text: "See you soon", from: "Alice"}
+  ];
+  // the symbolic property is only known to our code
+    let isRead = Symbol("isRead");
+    messages[0][isRead] = true;
+    let readMessages = new WeakSet();
+  
+  // two messages have been read
+  readMessages.add(messages[0]);
+  readMessages.add(messages[1]);
+  // readMessages has 2 elements
+  
+  // ...let's read the first message again!
+  readMessages.add(messages[0]);
+  // readMessages still has 2 unique elements
+  
+  // answer: was the message[0] read?
+  alert("Read message 0: " + readMessages.has(messages[0])); // true
+  
+  messages.shift();
+  // now readMessages has 1 element (technically memory may be cleaned later)
+
+  //Task 2
+  let messages = [
+    {text: "Hello", from: "John"},
+    {text: "How goes?", from: "John"},
+    {text: "See you soon", from: "Alice"}
+  ];
+  
+  let readMap = new WeakMap();
+  
+  readMap.set(messages[0], new Date(2017, 1, 1));
+  // Date object we'll study later
