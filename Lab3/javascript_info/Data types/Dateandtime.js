fae99c07e210afb57de85e8962d808a2c32247b1
Kamila <98703242+SakushK@users.noreply.github.com>
2023-02-16 17:08:22 +0600

    Create Dateandtime.js

---
+++ Lab3/javascript_info/Data types/Dateandtime.js
@@ -0,0 +1,102 @@
+// Task 1
+//new Date(datastring)
+let d2 = new Date("2012-02-20T03:12");
+alert( d2 );
+
+// Task 2
+function getWeekDay(date) {
+    let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+  
+    return days[date.getDay()];
+  }
+  
+  let date = new Date(2014, 0, 3); // 3 Jan 2014
+  alert( getWeekDay(date) ); // FR
+
+// Task 3
+let date = new Date(2012, 0, 3);  // 3 Jan 2012
+alert( getLocalDay(date) );       // tuesday, should show 2
+
+function getLocalDay(date) {
+
+    let day = date.getDay();
+  
+    if (day == 0) { // weekday 0 (sunday) is 7 in european
+      day = 7;
+    }
+  
+    return day;
+  }
+
+// Task 4
+function getDateAgo(date, days) {
+  let dateCopy = new Date(date);
+
+  dateCopy.setDate(date.getDate() - days);
+  return dateCopy.getDate();
+}
+
+let date = new Date(2015, 0, 2);
+
+alert( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
+alert( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
+alert( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
+
+// Task 5
+function getLastDayOfMonth(year, month) {
+    let date = new Date(year, month + 1, 0);
+    return date.getDate();
+  }
+  
+  alert( getLastDayOfMonth(2012, 0) ); // 31
+  alert( getLastDayOfMonth(2012, 1) ); // 29
+  alert( getLastDayOfMonth(2013, 1) ); // 28
+
+// Task 6
+function getSecondsToday() {
+  let d = new Date();
+  return d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
+}
+
+alert( getSecondsToday() );
+
+// Task 7
+function getSecondsToTomorrow() {
+    let now = new Date();
+  
+    // tomorrow date
+    let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1);
+  
+    let diff = tomorrow - now; // difference in ms
+    return Math.round(diff / 1000); // convert to seconds
+  }
+
+// Task 8
+function formatDate(date) {
+    let dayOfMonth = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let diffMs = new Date() - date;
+    let diffSec = Math.round(diffMs / 1000);
+    let diffMin = diffSec / 60;
+    let diffHour = diffMin / 60;
+  
+    // formatting
+    year = year.toString().slice(-2);
+    month = month < 10 ? '0' + month : month;
+    dayOfMonth = dayOfMonth < 10 ? '0' + dayOfMonth : dayOfMonth;
+    hour = hour < 10 ? '0' + hour : hour;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+  
+    if (diffSec < 1) {
+      return 'right now';
+    } else if (diffMin < 1) {
+      return `${diffSec} sec. ago`
+    } else if (diffHour < 1) {
+      return `${diffMin} min. ago`
+    } else {
+      return `${dayOfMonth}.${month}.${year} ${hour}:${minutes}`
+    }
+  }
