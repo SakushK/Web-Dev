e7680749b83be1a6bcdc46e91aef61ae65772ec1
Kamila <98703242+SakushK@users.noreply.github.com>
2023-02-16 17:17:52 +0600

    Create String.js

---
+++ Lab3/javascript_info/Data types/String.js
@@ -0,0 +1,27 @@
+//Task 1
+function ucFirst(str) {
+    if (!str) return str;
+  
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  
+  alert( ucFirst("john") ); // John
+//Task 2
+function checkSpam(str) {
+    let lowerStr = str.toLowerCase();
+  
+    return lowerStr.includes('viagra') || lowerStr.includes('xxx');
+  }
+  
+  alert( checkSpam('buy ViAgRA now') );
+  alert( checkSpam('free xxxxx') );
+  alert( checkSpam("innocent rabbit") );
+//Task 3
+function truncate(str, maxlength) {
+    return (str.length > maxlength) ?
+      str.slice(0, maxlength - 1) + '…' : str;
+  }
+//Task 4
+function extractCurrencyValue(str) {
+    return +str.slice(1);
+  }
