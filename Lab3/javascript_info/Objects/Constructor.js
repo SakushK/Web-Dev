3c647412a7b319a49f23922ea19b88cef4e2d055
Kamila <98703242+SakushK@users.noreply.github.com>
2023-02-16 17:23:08 +0600

    Create Constructor.js

---
+++ Lab3/javascript_info/Objects/Constructor.js
@@ -0,0 +1,54 @@
+//Task 1:Is it possible to create functions A and B so that new A() == new B()?
+
+// function A() { ... }
+// function B() { ... }
+
+// let a = new A();
+// let b = new B();
+
+// alert( a == b ); // true
+
+let obj = {};
+
+function A() { return obj; }
+function B() { return obj; }
+
+alert( new A() == new B() ); // true
+
+// Task 2:new calculator
+function Calculator() {
+
+    this.read = function() {
+      this.a = +prompt('a?', 0);
+      this.b = +prompt('b?', 0);
+    };
+  
+    this.sum = function() {
+      return this.a + this.b;
+    };
+  
+    this.mul = function() {
+      return this.a * this.b;
+    };
+  }
+
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
+
+//Task 3: accumulator
+function Accumulator(startingValue) {
+    this.value = startingValue;
+  
+    this.read = function() {
+      this.value += +prompt('How much to add?', 0);
+    };
+  
+  }
+  
+  let accumulator = new Accumulator(1);
+  accumulator.read();
+  accumulator.read();
+  alert(accumulator.value);
