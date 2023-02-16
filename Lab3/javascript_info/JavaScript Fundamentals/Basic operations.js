dc3d7be34ae3a488c324096d8eb37e5e26c40f81
Kamila <98703242+SakushK@users.noreply.github.com>
2023-02-16 15:50:31 +0600

    Create Basic operations.js

---
+++ Lab3/javascript_info/JavaScript Fundamentals/Basic operations.js
@@ -0,0 +1,22 @@
+//task 1
+let a = 1, b = 1;
+
+let c = ++a; // c=2, a=2
+let d = b++; // d=1, b=2
+
+//task 2
+let a = 2;
+
+let x = 1 + (a *= 2); // a=4, x=5
+
+//task 3
+let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
+
+alert(a + b); // 12
+
+let a = +prompt("First number?", 1);
+let b = +prompt("Second number?", 2);
+
+alert(a + b); // 3
+
