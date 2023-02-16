daf411fec98e820bcc7b7365da4a19481478260a
Kamila <98703242+SakushK@users.noreply.github.com>
2023-02-16 17:14:27 +0600

    Create Mapandset.js

---
+++ Lab3/javascript_info/Data types/Mapandset.js
@@ -0,0 +1,38 @@
+//Task 1
+function unique(arr) {
+    return Array.from(new Set(arr));
+    
+  }
+  
+  let values = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+  ];
+  
+  alert( unique(values) ); // Hare, Krishna, :-O
+//Task 2
+function aclean(arr) {
+    let obj = {};
+  
+    for (let i = 0; i < arr.length; i++) {
+      let sorted = arr[i].toLowerCase().split("").sort().join("");
+      obj[sorted] = arr[i];
+    }
+  
+    return Object.values(obj);
+  }
+  
+  let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+  
+  alert( aclean(arr) );
+
+//Task 3
+let map = new Map();
+
+map.set("name", "John");
+
+let keys = Array.from(map.keys());
+
+keys.push("more");
+
+alert(keys); // name, more
+s
