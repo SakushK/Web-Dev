763f6df4303b9b9c3b30dfbc98a1c30848cb23ad
Kamila <98703242+SakushK@users.noreply.github.com>
2023-02-16 17:05:57 +0600

    Create Arrays.js

---
+++ Lab3/javascript_info/Data types/Arrays.js
@@ -0,0 +1,65 @@
+//Task 1
+let fruits = ["Apples", "Pear", "Orange"];
+
+let shoppingCart = fruits;
+
+shoppingCart.push("Banana");
+
+alert( fruits.length ); // 4
+
+//Task 2
+let styles = ["Jazz", "Blues"];
+styles.push("Rock-n-Roll");
+styles[Math.floor((styles.length - 1) / 2)] = "Classics";
+alert( styles.shift() );
+styles.unshift("Rap", "Reggae");
+
+//Task 3
+let arr = ["a", "b"];
+
+arr.push(function() {
+  alert( this );
+})
+
+arr[2](); // a,b,function(){...}
+
+//Task 4
+function sumInput() {
+
+    let numbers = [];
+  
+    while (true) {
+  
+      let value = prompt("A number please?", 0);
+  
+      // should we cancel?
+      if (value === "" || value === null || !isFinite(value)) break;
+  
+      numbers.push(+value);
+    }
+  
+    let sum = 0;
+    for (let number of numbers) {
+      sum += number;
+    }
+    return sum;
+  }
+  
+  alert( sumInput() );
+
+//Task 5
+function getMaxSubSum(arr) {
+    let maxSum = 0;
+    let partialSum = 0;
+  
+    for (let item of arr) { // for each item of arr
+      partialSum += item; // add it to partialSum
+      maxSum = Math.max(maxSum, partialSum); // remember the maximum
+      if (partialSum < 0) partialSum = 0; // zero if negative
+    }
+  
+    return maxSum;
+  }
+  
+  alert( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+  alert( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
