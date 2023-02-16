9b09bcdfcfee60e853f658caf32fc7a6be106132
Kamila <98703242+SakushK@users.noreply.github.com>
2023-02-16 17:12:54 +0600

    Create Keysvalues.js

---
+++ Lab3/javascript_info/Data types/Keysvalues.js
@@ -0,0 +1,30 @@
+//Task 1
+function sumSalaries(salaries) {
+
+    let sum = 0;
+    for (let salary of Object.values(salaries)) {
+      sum += salary;
+    }
+  
+    return sum; // 650
+  }
+  
+  let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+  };
+  
+  alert( sumSalaries(salaries) ); // 650
+
+//Task 2
+let user = {
+    name: 'John',
+    age: 30
+  };
+
+  function count(obj) {
+    return Object.keys(obj).length;
+  }
+  
+  alert( count(user) ); // 2
