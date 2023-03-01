566aecd91e6b6951abb76452726b87e4bd630386
Kamila <98703242+SakushK@users.noreply.github.com>
2023-03-01 21:35:34 +0600

    Create product-list.component.ts

---
+++ Lab5/src/app/product-list/product-list.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+  selectedProduct?:string;
+
+ onSelect(a:string){
+  this.selectedProduct=a;
+ }
+ share(a : string) {
+  window.open("https://t.me/share/url?url=http://localhost:4200/&text="+a);
+}
+
+onNotify() {
+  window.alert('You will be notified when the product goes on sale');
+}
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
