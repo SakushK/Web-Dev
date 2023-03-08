9554858a38f7dbb76a716d42f2b5a89957cc1419
Kamila <98703242+SakushK@users.noreply.github.com>
2023-03-08 21:25:32 +0600

    Add files via upload

---
+++ Lab6/src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass']
+})
+export class HomeComponent {
+
+}
