a487de4d97ab81a426200e193eb85af4c69adfe3
Kamila <98703242+SakushK@users.noreply.github.com>
2023-04-12 20:59:40 +0600

    Create models.py

---
+++ Lab9/proj/api/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    city = models.CharField(max_length=100)
+    address = models.TextField()
+
+    class Meta:
+        verbose_name = 'Company'
+        verbose_name_plural = 'Companies'
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'city': self.city,
+            'address': self.address
+        }
+class Vacancy(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    salary = models.FloatField()
+    company = models.ForeignKey(Company, models.CASCADE, related_name='vacancies')
+
+    class Meta:
+        verbose_name = 'Vacancy'
+        verbose_name_plural = 'Vacancies'
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'salary': self.salary,
+            'company': self.company.to_json()
+        }
