e5f20716958480c5588c286fb0ecb46b53c529d6
Kamila <98703242+SakushK@users.noreply.github.com>
2023-04-19 21:48:43 +0600

    Add files via upload

---
+++ Lab10/proj/hh_back/api/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Company',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=255)),
+                ('description', models.TextField()),
+                ('city', models.CharField(max_length=150)),
+                ('address', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Vacancy',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=255)),
+                ('description', models.TextField()),
+                ('salary', models.FloatField(max_length=100)),
+                ('company', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='api.company')),
+            ],
+        ),
+    ]
